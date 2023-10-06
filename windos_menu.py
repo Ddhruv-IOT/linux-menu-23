1b6346a2f131953709176a95837bb292e396acf8
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-10-07 03:00:00 +0530

    windows menu under devlopment

---
+++ windos_menu.py
@@ -0,0 +1,137 @@
+import webbrowser
+import os
+import pyttsx3
+import twitter
+import wikipediaapi
+# import medium
+import smtplib
+
+def windows_menu():
+    def notepad():
+        os.system("notepad.exe")
+
+    def chrome():
+        webbrowser.open("https://www.google.com")
+
+    def whatsapp():
+        webbrowser.open("https://web.whatsapp.com/")
+
+    def email():
+        # Your email sending logic here
+        pass
+
+    def sms():
+        # Your SMS sending logic here
+        pass
+
+    def chatgpt():
+        # Your ChatGPT interaction logic here
+        pass
+
+    def geolocation():
+        # Your geolocation logic here
+        pass
+
+    def get_twitter_trends():
+        # Your Twitter trending topics logic here
+        api_key = 'YOUR_TWITTER_API_KEY'
+        api_secret_key = 'YOUR_TWITTER_API_SECRET_KEY'
+        access_token = 'YOUR_TWITTER_ACCESS_TOKEN'
+        access_token_secret = 'YOUR_TWITTER_ACCESS_TOKEN_SECRET'
+
+        auth = twitter.OAuthHandler(api_key, api_secret_key)
+        auth.set_access_token(access_token, access_token_secret)
+
+        api = twitter.API(auth)
+
+        trends = api.trends_place(id=1)  # Assuming worldwide trends
+        for trend in trends[0]['trends']:
+            print(trend['name'])
+
+    def get_hashtag_posts():
+        # Your hashtag posts retrieval logic here
+        pass
+
+    def get_wikipedia_data():
+        # Your Wikipedia data retrieval logic here
+        wiki_wiki = wikipediaapi.Wikipedia('en')
+        page_py = wiki_wiki.page("Python_(programming_language)")
+        print("Page text: %s" % page_py.text[:60])
+
+    def audio_player():
+        # Your audio player logic here
+        pass
+
+    def video_player():
+        # Your video player logic here
+        pass
+
+    def control_speaker_sound():
+        # Your speaker sound control logic here
+        pass
+
+    def post_linkedin():
+        # Your LinkedIn posting logic here
+        pass
+
+    def fill_form():
+        # Your form filling logic here
+        pass
+
+    while True:
+        # ... (unchanged menu loop)
+        print("\nMenu:")
+        print("1. Notepad")
+        print("2. Chrome")
+        print("3. WhatsApp")
+        print("4. Email")
+        print("5. SMS")
+        print("6. ChatGPT")
+        print("7. Geolocation")
+        print("8. Twitter Trends")
+        print("9. Hashtag Posts")
+        print("10. Wikipedia Data")
+        print("11. Audio Player")
+        print("12. Video Player")
+        print("13. Speaker Sound Control")
+        print("14. Post on LinkedIn")
+        print("15. Fill Form")
+        print("0. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            notepad()
+        elif choice == '2':
+            chrome()
+        elif choice == '3':
+            whatsapp()
+        elif choice == '4':
+            email()
+        elif choice == '5':
+            sms()
+        elif choice == '6':
+            chatgpt()
+        elif choice == '7':
+            geolocation()
+        elif choice == '8':
+            get_twitter_trends()
+        elif choice == '9':
+            get_hashtag_posts()
+        elif choice == '10':
+            get_wikipedia_data()
+        elif choice == '11':
+            audio_player()
+        elif choice == '12':
+            video_player()
+        elif choice == '13':
+            control_speaker_sound()
+        elif choice == '14':
+            post_linkedin()
+        elif choice == '15':
+            fill_form()
+        elif choice == '0':
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
